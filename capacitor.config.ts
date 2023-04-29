0e543584f872536fb3d18f5ae4c620a1d47e9342
mikoloism <mikoloism@gmail.com>
2023-04-29 13:48:07 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'yol',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
