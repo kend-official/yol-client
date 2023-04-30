2de45472ef1d636222e5e0bdb46d9368de7d68ff
Alireza Babaei <Alirezababaei@payand.org>
2023-04-30 11:48:54 +0330

    feat(component): create component for header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { IonHeader } from "@ionic/react";
+import { HeaderContent } from "./styled";
+
+export default function Header() {
+  return (
+    <IonHeader>
+      <HeaderContent>
+        <h4>yol</h4>
+      </HeaderContent>
+    </IonHeader>
+  );
+}
