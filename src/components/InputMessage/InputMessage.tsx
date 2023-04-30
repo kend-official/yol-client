9160c366acb160055d31d230670fcfb0daac8096
Alireza Babaei <Alirezababaei@payand.org>
2023-04-30 14:18:39 +0330

    feat(component): :rocket: create submit handler

--- src/components/InputMessage/InputMessage.tsx
+++ src/components/InputMessage/InputMessage.tsx
@@ -4,6 +4,9 @@ import { MutableRefObject, useRef } from "react";
 import { InputContainer } from "./styled";
 export default function InputMessage() {
   const RefInput = useRef<MutableRefObject<HTMLIonInputElement> | any>(null);
+  const handelSubmit = () => {
+    console.log(RefInput.current.value);
+  };
   return (
     <InputContainer>
       <IonInput
@@ -14,7 +17,7 @@ export default function InputMessage() {
         placeholder="Enter text"
       ></IonInput>
 
-      <IonButton>
+      <IonButton onClick={() => handelSubmit()}>
         <IonIcon slot="icon-only" icon={send}></IonIcon>
       </IonButton>
     </InputContainer>
