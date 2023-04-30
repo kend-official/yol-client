11de1311966d24bfa7137a8486d0013dca6b71ce
Alireza Babaei <Alirezababaei@payand.org>
2023-04-30 14:15:59 +0330

    feat(component): :zap: add input ref

--- src/components/InputMessage/InputMessage.tsx
+++ src/components/InputMessage/InputMessage.tsx
@@ -1,13 +1,15 @@
 import { IonButton, IonIcon, IonInput } from "@ionic/react";
 import { send } from "ionicons/icons";
+import { MutableRefObject, useRef } from "react";
 import { InputContainer } from "./styled";
-
 export default function InputMessage() {
+  const RefInput = useRef<MutableRefObject<HTMLIonInputElement> | any>(null);
   return (
     <InputContainer>
       <IonInput
         label="Message"
         labelPlacement="floating"
+        ref={RefInput}
         fill="solid"
         placeholder="Enter text"
       ></IonInput>
