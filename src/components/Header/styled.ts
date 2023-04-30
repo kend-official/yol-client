85600373cd1c8c91d304e2dac485f5cecb6c7e7d
Alireza Babaei <Alirezababaei@payand.org>
2023-04-30 11:47:56 +0330

    feat(styled): :lipstick: Migration css style to emation style

---
+++ src/components/Header/styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const HeaderContent = styled.div(() => ({
+  width: "100%",
+  height: "50p",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+}));
