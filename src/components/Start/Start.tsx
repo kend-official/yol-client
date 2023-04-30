9d6b0f9cf21b22f9671cba5228c8f4019535c038
Alireza Babaei <Alirezababaei@payand.org>
2023-04-30 12:17:52 +0330

    feat(component): add handleChange for get  group name

--- src/components/Start/Start.tsx
+++ src/components/Start/Start.tsx
@@ -1,13 +1,18 @@
-import { IonInput } from "@ionic/react";
+import { InputChangeEventDetail, IonInput } from "@ionic/react";
+import { FormEvent } from "react";
 
 import { Container } from "./styled";
 const StartChat: React.FC = () => {
+  const handleChange = (event: FormEvent<HTMLIonInputElement>) => {};
+
   return (
     <Container>
       <strong>Ready to start secret chat?</strong>
       <p>start with enter group name</p>
       <IonInput
         style={{ width: "20%" }}
+        autofocus={true}
+        onInput={(e: FormEvent<HTMLIonInputElement>) => handleChange(e)}
         label="Group name"
         label-placement="floating"
         fill="outline"
