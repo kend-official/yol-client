c0bd747a1866481cf6238a183274586559fad3ce
Alireza Babaei <Alirezababaei@payand.org>
2023-04-30 15:34:46 +0330

    feat(component): add share link chat component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,12 +1,14 @@
 import MessageCard from "components/CardMessage";
 import InputMessage from "components/InputMessage";
+import ShareLink from "components/ShareLink";
 import { MessageContainer, Wrapper } from "./styled";
 
 export default function Chat() {
   return (
     <Wrapper>
+      <ShareLink />
       <MessageContainer>
-        <MessageCard />
+        <MessageCard UserMessage={false} />
       </MessageContainer>
 
       <InputMessage />
